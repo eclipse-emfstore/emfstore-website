decd4b595164b20f4d87f6b14eeefa754d0a26c2
jonas <jonas>
2011-06-27 14:22:48 +0000

    updated download page

--- downloads.php
+++ downloads.php
@@ -11,7 +11,7 @@
  *******************************************************************************/
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
-	
+include( $_SERVER['DOCUMENT_ROOT'] . "/rap/_projectCommon.php" );	
 	$localVersion = false;
 	
 	# Define these here, or in _projectCommon.php for site-wide values
