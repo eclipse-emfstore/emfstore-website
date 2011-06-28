c9c03ff416c8c51c797141160db0fdf788c24a66
jonas <jonas>
2011-06-28 07:49:50 +0000

    download section

--- download.php
+++ download.php
@@ -17,8 +17,8 @@
 <h2>$pageTitle</h2>
 <p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
-<h3>Nightly Builds</h3>
-<p>EMFStore is moving to Eclipse. Currently we are finishing the IP Process. Until this is finished, we cannot provide Eclipse-conform releases. You can get the the latest nightly build from <a href="http://download.eclipse.org/emf-store/">this update site</a>. If you are waiting for a release, please <a href="communication.php"> contact us.</a></p>
+<h3>Milestone Builds</h3>
+<p>EMFStore is moving to Eclipse. Currently we in are the parallel IP Process. Until this is finished, we cannot provide a final release. You can get the the latest milestone build from <a href="http://download.eclipse.org/emf-store/">this update site (http://download.eclipse.org/emf-store/)</a>. If you have any questions, please do not hesitate <a href="communication.php"> contact us.</a></p>
 
 
 
