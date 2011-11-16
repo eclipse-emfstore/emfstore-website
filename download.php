1359ffc0d65e3d8d7f36f8ae0dd3954ea4853e49
jonas <jonas>
2011-11-16 11:41:01 +0000

    changed update site

--- download.php
+++ download.php
@@ -19,7 +19,11 @@
 <p>To install the EMF Store, you need a running Eclipse. The easiest way to get started is to use <a href="http://eclipse.org/downloads/packages/eclipse-modeling-tools/indigor">the latest modeling edition.</a></p>
 
 <h3>Milestone Builds</h3>
-<p>EMF Store has moved to Eclipse. Currently we are preparing our first official Eclipse release. Although, EMF Store has been developed 3 years before it moved to Eclipse. You can get the the latest milestone build from <a href="http://download.eclipse.org/emf-store/">this update site (http://download.eclipse.org/emf-store/)</a>. If you have any questions, please do not hesitate <a href="communication.php"> contact us.</a></p>
+<p>EMF Store has moved to Eclipse. Currently we are preparing our first official Eclipse release. Although, EMF Store has been developed 3 years before it moved to Eclipse. You can get the the latest milestone build from 
+<ul>
+<li><a href="http://download.eclipse.org/emf-store/089M905">0.8.9M905 (http://download.eclipse.org/emf-store/089M905)</a>.</li>
+</ul>
+If you have any questions, please do not hesitate <a href="communication.php"> contact us.</a></p>
 
 
 
