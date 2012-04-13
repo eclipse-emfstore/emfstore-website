5c9bb541490ed1aa02bad6958cf88b1ee5c35728
mhohenegger <mhohenegger>
2012-04-13 13:03:34 +0000

    new release process

--- download.php
+++ download.php
@@ -42,12 +42,16 @@ A log of new and noteworthy for every release is available <a href="http://wiki.
 
 <ul>
 <li><a href="http://download.eclipse.org/emf-store/milestones/latest">http://download.eclipse.org/emf-store/milestones/latest</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/latest/site.p2.zip">http://download.eclipse.org/emf-store/milestones/latest/site.p2.zip</a>.</li>
 </ul>
 
-<p>An archive of milestone builds is available here
+<p>A list of archived milestone builds is available here
 
 <ul>
-<li><a href="http://download.eclipse.org/emf-store/milestones/archive">http://download.eclipse.org/emf-store/milestones/archive</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M938">http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M938</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M938/site.p2.zip">http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M938/site.p2.zip</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M945">http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M945</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M945/site.p2.zip">http://download.eclipse.org/emf-store/milestones/archive/0.8.9.M945/site.p2.zip</a>.</li>
 </ul>
 
 A log of new and noteworthy for every release is available <a href="http://wiki.eclipse.org/EMFStore_-_News_and_Noteworthy">here</a>. <br/>
