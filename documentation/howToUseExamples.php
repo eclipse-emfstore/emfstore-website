f3f72d3988a3009caddf7e82ec89c29c826b2bb1
fpirchner <fpirchner>
2012-05-09 11:15:18 +0000

    Documentation EMFStore examples

---
+++ documentation/howToUseExamples.php
@@ -0,0 +1,21 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+/*******************************************************************************
+ * Copyright (c) 2009 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    
+ *******************************************************************************/
+
+
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="syntax-highlight-java.css"/>');
+	
+	$pageTitle 		= "EMF Store - How to use the examples";
+
+	$html  = $html  =file_get_contents('_howToUseExamples.html');
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
