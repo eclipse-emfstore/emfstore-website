262af5d5c65e243eecb236c1f161a1b5e06e3267
mkoegel <mkoegel>
2012-07-05 16:41:07 +0000

    fixed download link

--- download.php
+++ download.php
@@ -24,8 +24,8 @@
 <p>You can get the the latest release 0.9.3 from this update site:
 
 <ul>
-<li><a href="http://download.eclipse.org/emf-store/releases/latest">http://download.eclipse.org/emf-store/releases/lates</a>.</li>
-<li><a href="http://download.eclipse.org/emf-store/releases/latest/site.p2.zip">http://download.eclipse.org/emf-store/releases/lates/site.p2.zip</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/releases/latest">http://download.eclipse.org/emf-store/releases/latest</a>.</li>
+<li><a href="http://download.eclipse.org/emf-store/releases/latest/site.p2.zip">http://download.eclipse.org/emf-store/releases/latest/site.p2.zip</a>.</li>
 </ul>
 
 <p>An archive of release builds is available here:
