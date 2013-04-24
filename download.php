913490a861c3ec5d08685a92a90a31be2f881c91
Edgar Mueller <edgar.m.mueller@gmail.com>
2013-04-24 18:47:55 +0200

    Added RC2 download link

--- download.php
+++ download.php
@@ -23,10 +23,16 @@
 
 <p>Milestone builds reflect the most current development and are not as stable as release builds. You can get the the latest milestone build from this update site or download as zip file:
 
-<h6>1.0.0.RC1</h6>
+<h6>1.0.0.RC2</h6>
 <ul>
 <li><a href="http://download.eclipse.org/emf-store/milestones/latest">http://download.eclipse.org/emf-store/milestones/latest</a></li>
 <li><a href="http://download.eclipse.org/emf-store/milestones/latest/site.p2.zip">http://download.eclipse.org/emf-store/milestones/latest/site.p2.zip</a></li>
+</ul>
+
+<h6>1.0.0.RC1</h6>
+<ul>
+<li><a href="http://download.eclipse.org/emf-store/milestones/latest">http://download.eclipse.org/emf-store/milestones/archive/1.0.0RC1</a></li>
+<li><a href="http://download.eclipse.org/emf-store/milestones/latest/site.p2.zip">http://download.eclipse.org/emf-store/milestones/archive/1.0.0RC1/site.p2.zip</a></li>
 </ul>
 
 <p>A list of archived milestone builds is available here
